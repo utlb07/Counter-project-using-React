43827ceae06f9a5729faa8b9b40fc1b40bbbf95c
utlb07 <forzajuvemi07@gmail.com>
2023-11-26 00:38:22 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'; // Corrected the case of useState
 
 function App() {
+  const [counter, setCounter] = useState(0); // Corrected the case of useState
+  const addValue = () => {
+    setCounter(counter + 1);
+  }
+  const removeval = () => {
+    counter > 0 ? setCounter(counter - 1) : setCounter(0);
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Chai and React</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeval}>Decrease Value</button>
+    </>
   );
 }
 
